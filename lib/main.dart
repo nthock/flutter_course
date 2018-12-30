3d6413bb44e7e26492731d6789a738812476baec
Thiam Hock Ng <nthock@gmail.com>
2018-12-30 17:20:05 +0800

    Add pages and list view

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import './product_manager.dart';
+import './pages/home.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,14 +11,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           brightness: Brightness.light,
           primarySwatch: Colors.deepOrange,
-          accentColor: Colors.deepPurple
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('EasyList'),
-        ),
-        body: ProductManager('Food Tester'),
-      ),
+          accentColor: Colors.deepPurple),
+      home: HomePage(),
     );
   }
 }
